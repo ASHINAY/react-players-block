6925cbc35e8f3896642ff21f29b1ea1ab4c59397
ASHINAY <anujamtech@gmail.com>
2023-12-19 21:50:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="root">
+      <div className="Main-block">
+        <div className="block-1">
+          <div className="text1">
+          Stage Name
+          </div>
+          <div className="text2">
+            Qualifiers
+          </div>
+        </div>
+        <div className="block-1">
+          <div className="text1" >
+            Format
+          </div>
+          <div className="text2">
+            Squad
+          </div>
+        </div>
+        <div className="block-1">
+          <div className="text1">
+            Players
+          </div>
+          <div  className="text2">
+            210
+          </div>
+        </div>
+        <div className="block-1">
+          <div className="text1">
+            Players Qualifying
+          </div>
+          <div className="text2">
+            80
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constant.js
@@ -0,0 +1,8 @@
+
+// export const quotes ="Nature does not hurry, yet everything is accomplished."
+
+export const buttonNames = [
+    "Desktop Button 1",
+    "Desktop Button 2",
+    "Desktop Button 3",
+  ];
